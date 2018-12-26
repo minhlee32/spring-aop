d44b64b9e6b56b6ae8f6ff50a0b1b0d3eb03a57b
minh <lengocminh@unimarmores.com>
2018-12-26 09:15:39 +0700

    logging aspect

---
+++ src/main/java/com/spring/demo/LoggingAspect.java
@@ -0,0 +1,15 @@
+package com.spring.demo;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* add*())")
+    public void beforeAddAccountAdvice(){
+        System.out.println("\n=====>> Executing @Before advice on addAccount()");
+    }
+}
